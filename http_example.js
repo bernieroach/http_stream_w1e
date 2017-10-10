ca35b2272f8241ccc38a6c90eff5a854e7b32a34
Bernard Roach <bernieroach@gmail.com>
2017-10-10 23:18:12 +0000

    commiting because it says so again callback function

--- http_example.js
+++ http_example.js
@@ -7,12 +7,21 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+// var callback = function() {
+//   console.log('In response handler callback!');
+// }
+
+// called by https when the request is made.
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
+
+//callback = null;
 
 console.log("I'm about to make the request!");
 
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
